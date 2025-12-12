373d8bd6f0e51c36a654c3ff6628c98e42641ab9
Carlos Eduardo da Silva Oliveira <48035815+KDZsilva@users.noreply.github.com>
2025-12-12 17:05:12 -0300

    feat: create navbar's test page

---
+++ resources/views/navbarteste.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        @vite('resources/css/app.css')
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
+        <link rel="stylesheet" href="{{asset('css/google.css')}}">
+    </head>
+    <body class="bg-[#ffffff] dark:bg-[#ffffff] text-[#1b1b18] flex w-screen h-screen">
+        <x-navbar />
+    </body>
